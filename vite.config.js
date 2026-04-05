152c64e2443f82e110c348dfe64c0e01b0beab31
Silambarsan <developersimbu2025@gmail.com>
2026-04-05 18:46:03 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React_Vite_CICD_Github/',
-  
-})
+  base: "/React_Vite_CICD/",
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
