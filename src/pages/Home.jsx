0e6af65cbe25f26f7e63fd9eba4d58a0204d7cce
Silambarsan <developersimbu2025@gmail.com>
2026-04-05 18:11:51 +0530

    fixed merge conflict

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Home = () => {
-<<<<<<< HEAD
-  return <div>STR Welcome cicd</div>;
-=======
-  return <div>React_Docker_CICD_Github simbu</div>;
->>>>>>> b2fa0427252dd322ebb4dde455ce1d302ce6c4a2
+  return <div>Home</div>;
 };
 
 export default Home;
