c948b095f5995305398daed53b6cf4feb780ea5e
Silambarsan <developersimbu2025@gmail.com>
2026-04-06 06:19:28 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>git-action</h1>
-      React js + Vite + Tailwind CSS + DaisyUI + React Router + React Query +
-      React Hook Form + Zod + Axios + Prettier + Eslint + Husky + Lint Staged +
-      Commitlint + EditorConfig + Vite Plugin PWA + Vite Plugin SVGR + Vite
-      Plugin Compression + Vite Plugin Image Minify + Vite Plugin Bundle
-      Analyzer + Vite Plugin Env Compatible + Vite Plugin HTML + Vite Plugin
-      Legacy + Vite Plugin React Refresh + Vite Plugin TypeScript Checker
+      <h4>git-action</h4>
+      <p>
+        React js + Vite + Tailwind CSS + DaisyUI + React Router + React Query +
+        React Hook Form + Zod + Axios + Prettier + Eslint + Husky + Lint Staged
+        + Commitlint + EditorConfig + Vite Plugin PWA + Vite Plugin SVGR + Vite
+        Plugin Compression + Vite Plugin Image Minify + Vite Plugin Bundle
+        Analyzer + Vite Plugin Env Compatible + Vite Plugin HTML + Vite Plugin
+        Legacy + Vite Plugin React Refresh + Vite Plugin TypeScript Checker
+      </p>
     </>
   );
 }
